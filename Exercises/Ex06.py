accf25df357864222503ddb63eb0117085f26983
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-10-15 11:29:34 -0300

    Ex06

---
+++ Exercises/Ex06.py
@@ -0,0 +1,7 @@
+import math
+
+n = int(input("Digite um número: "))
+
+print("O dobro de {} vale {}".format(n,(n * 2)))
+print("O triplo de {} vale {}".format(n,(n * 3)))
+print("A raíz quadrada de {} é igual a {:.2f}".format(n,(math.sqrt(n))))
